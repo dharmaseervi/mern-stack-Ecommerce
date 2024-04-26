fbc3f8ac23d82bea423acf65f71258a09caa2e1c
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-04-27 03:41:34 +0530

    change link component href formatting

--- components/ProfileDropDown.js
+++ components/ProfileDropDown.js
@@ -37,7 +37,7 @@ export default function ProfileDropDown() {
                         <Menu.Item>
                             {({ active }) => (
                                 <Link
-                                    href={"/useraccount"}
+                                    href="/useraccount"
                                     className={classNames(
                                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                         'block px-4 py-2 text-sm'
@@ -47,6 +47,7 @@ export default function ProfileDropDown() {
                                 </Link>
                             )}
                         </Menu.Item>
+                        console.log("After MENU Link")
                         <Menu.Item>
                             {({ active }) => (
                                 <Link
