3dd15f493b6fef85ba9048b31416834f8f32071a
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-04-29 13:09:12 +0530

    component update changes

--- components/ProfileDropDown.js
+++ components/ProfileDropDown.js
@@ -37,7 +37,7 @@ export default function ProfileDropDown() {
                         <Menu.Item>
                             {({ active }) => (
                                 <Link
-                                    href={"/useraccounts"}
+                                    href="/useraccounts"
                                     className={classNames(
                                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                         'block px-4 py-2 text-sm'
