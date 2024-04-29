757ab84375bcd27924dfbf81c5fb86cd9a904d37
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-04-29 13:01:16 +0530

    new component add test

--- components/ProfileDropDown.js
+++ components/ProfileDropDown.js
@@ -50,7 +50,7 @@ export default function ProfileDropDown() {
                         <Menu.Item>
                             {({ active }) => (
                                 <Link
-                                    href="#"
+                                    href="/support"
                                     className={classNames(
                                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                         'block px-4 py-2 text-sm'
