734eed303279e6d83f0ea121c35467757f135812
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-04-29 13:52:33 +0530

    component update changes bug fix

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -21,7 +21,7 @@ export default function SideNavbar() {
                 user account
             </div>
             <nav className='flex flex-col gap-3'>
-                <Link href='/useraccounts' className={`${pathname === '/useraccount' ? active : null} text-black flex gap-1 font-serif font-medium text-xl border border-black p-2 rounded hover:text-black hover:bg-white  `}>
+                <Link href="/useraccounts" className={`${pathname === '/useraccount' ? active : null} text-black flex gap-1 font-serif font-medium text-xl border border-black p-2 rounded hover:text-black hover:bg-white  `}>
                     <div className='flex'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -33,7 +33,7 @@ export default function SideNavbar() {
                     </svg> : null
                     }
                 </Link>
-                <Link href='/order' className={`${pathname === '/order' ? active : null}  text-black flex gap-1 font-serif font-medium text-xl border border-black p-2 rounded hover:text-black hover:bg-white  `}>
+                <Link href="/order" className={`${pathname === '/order' ? active : null}  text-black flex gap-1 font-serif font-medium text-xl border border-black p-2 rounded hover:text-black hover:bg-white  `}>
                     <div className="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
@@ -46,7 +46,7 @@ export default function SideNavbar() {
                     }
                 </Link>
 
-                <Link href='/address' className={`${pathname === '/address' ? active : null}  text-black flex gap-1 font-serif font-medium text-xl border border-black p-2 rounded hover:text-black hover:bg-white  `}>
+                <Link href="/address" className={`${pathname === '/address' ? active : null}  text-black flex gap-1 font-serif font-medium text-xl border border-black p-2 rounded hover:text-black hover:bg-white  `}>
                     <div className='flex'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
