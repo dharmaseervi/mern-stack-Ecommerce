3d9981d8d148c849303c296848175a5fd4174720
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-05-04 01:58:56 +0530

    bug fixes

--- middleware.js
+++ middleware.js
@@ -2,29 +2,29 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const path = request.nextUrl.pathname;
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-  const privatePaths = ["/useraccounts", "/userorder", "/savedaddress", "/signup"]; // Add your private paths here
+  // const path = request.nextUrl.pathname;
+  // const token = await getToken({
+  //   req: request,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  // });
+  // const privatePaths = ["/useraccounts", "/userorder", "/savedaddress", "/signup"]; // Add your private paths here
 
-  if (!token && privatePaths.includes(path)) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
-  // Check if the path starts with '/register/'
-  if (!token && request.nextUrl.pathname.startsWith("/sign-up/")) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
+  // if (!token && privatePaths.includes(path)) {
+  //   return NextResponse.redirect(new URL("/", request.nextUrl));
+  // }
+  // // Check if the path starts with '/register/'
+  // if (!token && request.nextUrl.pathname.startsWith("/sign-up/")) {
+  //   return NextResponse.redirect(new URL("/", request.nextUrl));
+  // }
 }
 
-export const config = {
-  // Matcher for all paths
-  matcher: ({ req }) => {
-    // Match all routes except for API routes and private paths
-    const privatePaths = ["/useraccounts", "/userorder", "/savedaddress", "/signup"];
-    const isPrivatePath = privatePaths.some(path => req.url.startsWith(path));
-    return !req.url.startsWith("/api") && isPrivatePath;
-  },
-};
+// export const config = {
+//   // Matcher for all paths
+//   matcher: ({ req }) => {
+//     // Match all routes except for API routes and private paths
+//     const privatePaths = ["/useraccounts", "/userorder", "/savedaddress", "/signup"];
+//     const isPrivatePath = privatePaths.some(path => req.url.startsWith(path));
+//     return !req.url.startsWith("/api") && isPrivatePath;
+//   },
+// };
 
